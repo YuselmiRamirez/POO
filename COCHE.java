f0b6d70bce1d0fb9cfc36fbc00ce9fe5b854546c
YuselmiRamirez <142958554+YuselmiRamirez@users.noreply.github.com>
2026-02-23 02:26:33 +0000

    correccion de errores

--- COCHE.java
+++ COCHE.java
@@ -1,33 +1,40 @@
-public class COCHE{
-    string color;
-    string modelo;
-    string velocidad;
-    public COCHE(string color, string modelo) {
+public class COCHE {
+    String color;
+    String modelo;
+    int velocidad;
+
+    public COCHE(String color, String modelo) {
         this.color = color;
         this.modelo = modelo;
         this.velocidad = 0;
     }
-    public void ACELERAR(int incremento){
+
+    public void ACELERAR(int incremento) {
         this.velocidad += incremento;
-        system.out.println("El coche ahora va a " + this.velocidad + "KM/H.");
+        System.out.println("El coche ahora va a " + this.velocidad + " KM/H.");
     }
-    public void FRENAR(int decremento){
+
+    public void FRENAR(int decremento) {
         this.velocidad -= decremento;
-        if(this.velocidad < 0){
+        if (this.velocidad < 0) {
             this.velocidad = 0;
         }
-        system.out.println("El coche ahora va a " + this.velocidad + "KM/H.");
+        System.out.println("El coche ahora va a " + this.velocidad + " KM/H.");
     }
-    public string OBTENERDESCRIPCION(){
+
+    public String OBTENERDESCRIPCION() {
         return "Este es un " + this.modelo + " de color " + this.color + ".";
     }
-    public static void main(string[] args){
+
+    public static void main(String[] args) {
         COCHE COCHE1 = new COCHE("Rojo", "Ford Mustang");
         COCHE COCHE2 = new COCHE("Azul", "Tesla Model S");
-        system.out.println(COCHE1.OBTENERDESCRIPCION());
+
+        System.out.println(COCHE1.OBTENERDESCRIPCION());
         COCHE1.ACELERAR(50);
         COCHE1.FRENAR(10);
-        system.out.println("\n" + COCHE2.OBTENERDESCRIPCION());
+
+        System.out.println("\n" + COCHE2.OBTENERDESCRIPCION());
         COCHE2.ACELERAR(100);
     }
 }
