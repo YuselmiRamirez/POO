496483c562787186595d2ed7f030cf14912cf45e
YuselmiRamirez <142958554+YuselmiRamirez@users.noreply.github.com>
2026-03-05 16:58:28 +0000

    programa rpg

---
+++ Notificacion.java
@@ -0,0 +1,15 @@
+public class Notificacion {
+
+    public mostrar(String emisor, String mensaje) {
+        this.emisor = emisor;
+        this.mensaje = mensaje;
+    }
+
+public void LogLogro(puntosExperiencia) {
+    puntosExperiencia += incremento;
+    System.out.println("LOGRO DESBLOQUEADO! " + emisor + "dice" + mensaje + ". Recompensa: " + puntosExperiencia + " [XP] pts."
+    }
+public void AlertaMision(NivelDificultad) {
+    
+}
+}
